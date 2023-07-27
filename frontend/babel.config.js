ae28efd36db420623a902a66fa4d9bc6912cd85a
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-07-27 16:47:16 +0900

    Feat : test
    npm test 로 중복검사 test해보기

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  'presets': ['@babel/preset-env']
-}
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current", // 또는 원하는 타겟 환경 설정
+        },
+      },
+    ],
+  ],
+  plugins: ["@babel/plugin-transform-unicode-sets-regex"],
+};
